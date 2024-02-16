ff771284dcfedf10683bec0f4d66e2f25dc85629
Rafazanella01 <rafazanella2004@gmail.com>
2024-02-16 09:04:37 -0300

    Add Exercicio 3

--- app/Models/StringManipulator.php
+++ app/Models/StringManipulator.php
@@ -8,4 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class StringManipulator extends Model
 {
     use HasFactory;
+
+    public function countVowels(string $str)
+    {
+        $count = 0;
+        $vogais = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+
+        $letras = str_split($str);
+
+        foreach ($letras as $letra){
+            if(in_array($letra, $vogais)){
+                $count++;
+            }
+        }
+
+        return $count;
+    }
+
+    public function capitalizeString(string $string)
+    {
+        if (empty($string)){
+            return $string;
+        }
+
+        return ucwords($string);;
+    }
+
+    public function concatenateStrings(string $string1, string $string2)
+    {
+        return $string1 . ' ' . $string2;
+    }
 }
--- tests/Unit/StringManipulatorTest.php
+++ tests/Unit/StringManipulatorTest.php
@@ -2,12 +2,85 @@
 
 namespace Tests\Unit;
 
-use App\Models\Soma;
+use App\Models\StringManipulator;
 use PHPUnit\Framework\TestCase;
 
     class StringManipulatorTest extends TestCase
     {
-        
+
+        // Testes Vogais
+
+        public function testVogaisSemVogais()
+        {
+            $texto = '28092004';
+            $classe = new StringManipulator();
+
+            $vogais = $classe->countVowels($texto);
+
+            $this->assertEquals(0, $vogais);
+        }
+
+        public function testVogaisMinuscula()
+        {
+            $texto = 'aeiou';
+            $classe = new StringManipulator();
+
+            $vogais = $classe->countVowels($texto);
+
+            $this->assertEquals(5, $vogais);
+        }
+
+        public function testVogaisMaiuscula()
+        {
+            $texto = 'AEIOU';
+            $classe = new StringManipulator();
+
+            $vogais = $classe->countVowels($texto);
+
+            $this->assertEquals(5, $vogais);
+        }
+
+        public function testVogaisMinusculaMaiuscula()
+        {
+            $texto = 'AEioU';
+            $classe = new StringManipulator();
+
+            $vogais = $classe->countVowels($texto);
+
+            $this->assertEquals(5, $vogais);
+        }
+
+        public function testVogais()
+        {
+            $texto = 'Oii Rafa';
+            $classe = new StringManipulator();
+
+            $vogais = $classe->countVowels($texto);
+
+            $this->assertEquals(5, $vogais);
+        }
+
+        // Capitalize strings
+
+        public function testCapitalizeStrings()
+        {
+            $texto = 'o rafa é lindo';
+            $classe = new StringManipulator();
+
+            $capitalize = $classe->capitalizeString($texto);
+            $this->assertEquals('O Rafa é Lindo', $capitalize);
+        }
+
+        public function testConcatenateStrings()
+        {
+            $texto1 = 'Meu nome é';
+            $texto2 = 'Rafael';
+
+            $classe = new StringManipulator();
+            $Concatenador = $classe->concatenateStrings($texto1, $texto2);
+
+            $this->assertEquals('Meu nome é Rafael', $Concatenador);
+        }
     }
 
 ?> 
